b293ab6be4df189334509c74c70ccc98b7d415b0
Andrii Kotsiuruba <akotsiuruba.codica@gmail.com>
2024-07-31 01:08:04 +0300

    [Feature] Frontend

--- babel.config.js
+++ babel.config.js
@@ -44,7 +44,7 @@ module.exports = function(api) {
       [
         '@babel/plugin-proposal-class-properties',
         {
-          loose: true
+          loose: false
         }
       ],
       [
@@ -54,21 +54,11 @@ module.exports = function(api) {
         }
       ],
       [
-        '@babel/plugin-proposal-private-methods',
-        {
-          loose: true
-        }
-      ],
-      [
-        '@babel/plugin-proposal-private-property-in-object',
-        {
-          loose: true
-        }
-      ],
-      [
         '@babel/plugin-transform-runtime',
         {
-          helpers: false
+          helpers: false,
+          regenerator: true,
+          corejs: false
         }
       ],
       [
